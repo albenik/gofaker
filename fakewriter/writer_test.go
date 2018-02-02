cb40fe69410379cd0798932a7b588de06b399a24
Veniamin Albaev <albenik@gmail.com>
2018-02-02 12:35:00 +0300

    Tests fixed

--- fakewriter/writer_test.go
+++ fakewriter/writer_test.go
@@ -21,7 +21,7 @@ func TestWriter_EOF(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "unexpected 1 write", tt.FailMessage)
+	assert.Equal(t, "write #1: unexpected [01]", tt.FailMessage)
 }
 
 func TestWriter_Locked(t *testing.T) {
@@ -37,13 +37,13 @@ func TestWriter_Locked(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data length: 101 expected but 1 recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data length: 101 expected but 1 recieved [01]", tt.FailMessage)
 
 	n, err = w.Write([]byte{1})
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "writer locked at 2 write", tt.FailMessage)
+	assert.Equal(t, "write #2: locked [01]", tt.FailMessage)
 }
 
 func TestAssertLen_OK(t *testing.T) {
@@ -77,7 +77,7 @@ func TestAssertLen_MismatchNonzero(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 2, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data length: 3 expected but 2 recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data length: 3 expected but 2 recieved [01 02]", tt.FailMessage)
 }
 
 func TestAssertLen_MismatchZero(t *testing.T) {
@@ -91,7 +91,7 @@ func TestAssertLen_MismatchZero(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data length: 3 expected but 0 recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data length: 3 expected but 0 recieved []", tt.FailMessage)
 }
 
 func TestAssertLen_MismatchNil(t *testing.T) {
@@ -105,7 +105,7 @@ func TestAssertLen_MismatchNil(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data length: 3 expected but 0 recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data length: 3 expected but 0 recieved []", tt.FailMessage)
 }
 
 func TestAssertData_OK(t *testing.T) {
@@ -139,7 +139,7 @@ func TestAssertData_MismatchNonempty(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 1, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data: [01 02 03] expected but [01] recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data: [01 02 03] expected but [01] recieved", tt.FailMessage)
 }
 
 func TestAssertData_MismatchEmpty(t *testing.T) {
@@ -153,7 +153,7 @@ func TestAssertData_MismatchEmpty(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data: [01 02 03] expected but [] recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data: [01 02 03] expected but [] recieved", tt.FailMessage)
 }
 
 func TestAssertData_MismatchZero(t *testing.T) {
@@ -167,7 +167,7 @@ func TestAssertData_MismatchZero(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 0, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data: [01 02 03] expected but [] recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data: [01 02 03] expected but [] recieved", tt.FailMessage)
 }
 
 func TestShortWrite_Short_Success(t *testing.T) {
@@ -210,7 +210,7 @@ func TestShortWrite_FailLen(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 3, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data length: 2 expected but 3 recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data length: 2 expected but 3 recieved [03 02 01]", tt.FailMessage)
 }
 
 func TestShortWrite_FailData(t *testing.T) {
@@ -223,7 +223,7 @@ func TestShortWrite_FailData(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, 3, n)
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data: [01 02 03] expected but [03 02 01] recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data: [01 02 03] expected but [03 02 01] recieved", tt.FailMessage)
 }
 
 func TestDelayWrite_Success(t *testing.T) {
@@ -260,7 +260,7 @@ func TestDelayWrite_Fail(t *testing.T) {
 	assert.Equal(t, 1, n)
 	assert.Equal(t, now.Add(15*time.Second), clk.Now())
 	assert.True(t, tt.FailedAsExpected)
-	assert.Equal(t, "invalid data: [01 02 03] expected but [01] recieved", tt.FailMessage)
+	assert.Equal(t, "write #1: invalid data: [01 02 03] expected but [01] recieved", tt.FailMessage)
 }
 
 func TestWriteError(t *testing.T) {
