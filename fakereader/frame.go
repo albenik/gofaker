e3683e69d6b4e58099b06074ef8c49314e1d68c6
Veniamin Albaev <albenik@gmail.com>
2018-02-25 23:36:47 +0300

    fakereader.Frame improvements

--- fakereader/frame.go
+++ fakereader/frame.go
@@ -13,8 +13,39 @@ func NewFrame(p []byte) *Frame {
 	return f
 }
 
+func (f *Frame) Restart() *Frame {
+	f.offs = 0
+	return f
+}
+
 func (f *Frame) Chunk(ln int) io.Reader {
-	r := EqualData(f.data[f.offs : f.offs+ln])
-	f.offs += ln
+	end := f.offs + ln
+	if end > len(f.data) {
+		panic("not enougth data")
+	}
+	r := EqualData(f.data[f.offs:end])
+	f.offs = end
 	return r
+}
+
+func (f *Frame) MultipleChunks(ln, cnt int) []io.Reader {
+	if f.offs+ln*cnt > len(f.data) {
+		panic("not enougth data")
+	}
+	list := make([]io.Reader, cnt)
+	for i := 0; i < cnt; i++ {
+		list[i] = f.Chunk(ln)
+	}
+	return list
+}
+
+func (f *Frame) AllChunks(ln int) []io.Reader {
+	if f.offs >= len(f.data) {
+		panic("not enougth data")
+	}
+	remain := len(f.data) - f.offs
+	if remain%ln != 0 {
+		panic("data chunks not aligned")
+	}
+	return f.MultipleChunks(ln, remain/ln)
 }
